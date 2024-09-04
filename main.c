2744d51fcd3fb9ef6b77a848b8fca5d4ece5ae6b
LJAMINE <125272205+LJAMINE@users.noreply.github.com>
2024-09-04 23:21:28 +0100

    Add files via upload
    table de mltilication

--- main.c
+++ main.c
@@ -3,24 +3,13 @@
 
 int main()
 {
-    int tableau [5] ={1,3,11,4,5};
-    int a=0 ;
-    int b=0 ;
-   int pair[5] ;
-   int  impair[5] ;
- for (int i=0;i<5;i++){
-         if(tableau[i]%2==0){
-                pair[a]=tableau[i];
-              printf("les nombres pair sont %d \n",pair[a]);
-            a++;
-        }else{
-        impair[b]=tableau[i];
-                    printf("les nombres impair   %d\n",impair[b]);
-                    b++;
+ int i , nombre;
 
+ printf("entrer nombre ");
+ scanf("%d",&nombre);
 
-        }
-
-}
+ for (int i=0;i<10;i++){
+    printf("%d * %d =%d\n",nombre,i,nombre*i);
+ }
      return 0;
 }
